a79c0463045e4b124bd01dfe1076d0d76efa0dfb
Riccardo <rik.todaro@gmail.com>
2026-04-04 20:41:21 +0200

    FEATURE: improved test site with js and css

--- www/cgi-bin/echo.py
+++ www/cgi-bin/echo.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python3
 
-import os
 import sys
 
 print("Content-Type: text/html\n")
 
-print("<html><body>")
-print("<h1>Echo CGI</h1>")
-
-print("<h2>Environment:</h2>")
-for k, v in os.environ.items():
-    print(f"{k} = {v}<br>")
-
-print("<h2>Body:</h2>")
 data = sys.stdin.read()
-print(data)
 
+print("<html><body>")
+print("<h1>POST DATA:</h1>")
+print("<pre>" + data + "</pre>")
 print("</body></html>")
--- www/cgi-bin/hello.py
+++ www/cgi-bin/hello.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 
 print("Content-Type: text/html\n")
+
 print("<html><body>")
-print("<h1>Hello from Python CGI 🐍</h1>")
+print("<h1>🐍 Python CGI works!</h1>")
 print("</body></html>")
